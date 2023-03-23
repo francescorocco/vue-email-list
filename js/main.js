0d34caadd1815f104e45e8bdb50c894716ed155c
Francesco Rocco <francescorocco97@gmail.com>
2023-03-23 16:24:00 +0100

    Esercizio completato con qualche bonus

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        emails:[],
+      }
+    },
+    mounted() {
+      for(let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+          this.emails.push(response.data.response);
+        }
+        );
+      }
+    }
+  }).mount('#app')
